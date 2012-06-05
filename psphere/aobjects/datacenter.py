c115b873d2c725388ad266ee042b5a20c2699f70
intr1nsic <juice@tallshorts.com>
2012-06-05 01:19:15 -0400

    Added the ability to create a cluster on the aobjects.Datacenter
    Because this is not instantiated from the class method, you will need to call the aobject directly.
    
    IE: from psphere.aobjects.datacenter import Datacenter
    
    This does break the model of being able to just `from psphere.managedobjects import Datacenter`. This was done just in case if psphere gets updated, we won't break the upgrade path.

--- psphere/aobjects/datacenter.py
+++ psphere/aobjects/datacenter.py
@@ -1,9 +1,12 @@
 from psphere import managedobjects
 from psphere.network import utils
 
+import inspect
+
 class Datacenter(managedobjects.Datacenter):
 	def __init__(self, mo_ref, client):
 		managedobjects.Datacenter.__init__(self, mo_ref, client)
+		self.client = client
 
 	def add_vlan(self, vlan, cidr):
 		"""
@@ -15,6 +18,36 @@ class Datacenter(managedobjects.Datacenter):
 		:type cidr: str
 		"""
 		self.vlans[vlan] = utils.Vlan(vlan, cidr)
+
+	@classmethod
+	def create_dc(cls, client, name=None, folder=None):
+		"""
+		Create a datacenter. Optionally you can include
+		a mo_ref to a Folder in which you want to create
+		the datacenter
+
+		:param folder: mo_ref to the folder for the datacenter
+		:type folder: mo_ref
+		"""
+
+		if folder:
+			# Test to make sure its a ref
+			if not hasattr(folder, "_mo_ref"):
+				raise Exception("Folder is not a mo_ref.")
+			else:
+				rootFolder = folder
+		else:
+			rootFolder = client.sc.rootFolder
+
+		try:
+			dc = rootFolder.CreateDatacenter(name=name)
+			return dc
+		except Exception, e:
+			raise e
+
+
+		
+
 
 """ Begin Testing """
 def datacenter_test():
